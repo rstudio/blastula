3be3c87d90a7e99868bdb5a86b9b705eb747fe1c
Richard Iannone <riannone@me.com>
2017-11-18 22:11:00 -0800

    Use inlined version of HTML

--- R/add_cta_button.R
+++ R/add_cta_button.R
@@ -9,8 +9,8 @@
 #' the CTA button.
 #' @param align the alignment of the button
 #' inside the main content area. Options are
-#' \code{left} (the default), \code{right},
-#' and \code{center}.
+#' \code{center} (the default), \code{left},
+#' and \code{right}.
 #' @return a character object with an HTML
 #' fragment that can be placed inside the
 #' message body wherever the CTA button
@@ -20,8 +20,7 @@
 #' cta_button <-
 #'   add_cta_button(
 #'     url = "http://www.website.net",
-#'     text = "Press This Button",
-#'     align = "center")
+#'     text = "Press This Button")
 #'
 #' # Include the button in the email
 #' # message body by simply referencing
@@ -43,24 +42,25 @@
 
 add_cta_button <- function(url,
                            text,
-                           align = "left") {
+                           align = "center") {
 
   glue::glue(
     "
-    <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"btn btn-primary\">
-    <tbody>
-    <tr>
-    <td align=\"{align}\">
-    <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\">
-    <tbody>
-    <tr>
-    <td> <a href=\"{url}\" target=\"_blank\">{text}</a> </td>
-    </tr>
-    </tbody>
-    </table>
-    </td>
-    </tr>
-    </tbody>
-    </table>
-    ")
+  <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"btn btn-primary\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; box-sizing: border-box;\">
+  <tbody>
+  <tr>
+  <td align=\"{align}\" style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; padding-bottom: 15px;\">
+  <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;\">
+  <tbody>
+  <tr>
+  <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;\"> <a href=\"{url}\" target=\"_blank\" style=\"display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-transform: capitalize; border-color: #3498db;\">{text}</a> </td>
+  </tr>
+  </tbody>
+  </table>
+  </td>
+  </tr>
+  </tbody>
+  </table>
+ ")
+
 }
