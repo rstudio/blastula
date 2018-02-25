b62929736423b1409174a60cf4afe6849c67a79c
rich-iannone <riannone@me.com>
2018-02-25 01:27:41 -0500

    Add `utils.R` file

---
+++ R/utils.R
@@ -0,0 +1,11 @@
+# SMTP providers with settings
+smtp_settings <- function() {
+  dplyr::tribble(
+    ~short_name,  ~server,                   ~port, ~use_ssl, ~use_tls, ~authenticate, ~long_name,
+    "gmail",      "smtp.gmail.com",          465,   TRUE,     FALSE,    TRUE,          "Gmail",
+    "outlook",    "smtp-mail.outlook.com",   587,   FALSE,    TRUE,     TRUE,          "Outlook.com",
+    "office365",  "smtp.office365.com",      587,   FALSE,    TRUE,     TRUE,          "Office365.com",
+    "icloud",     "smtp.mail.me.com",        587,   FALSE,     TRUE,    TRUE,          "iCloud Mail"
+  )
+}
+
