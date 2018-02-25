c7ede1bc4dd8c1d0cb1041193578252d5eef215f
rich-iannone <riannone@me.com>
2018-02-25 01:28:37 -0500

    Add option to use an SMTP provider name

--- R/create_email_creds_file.R
+++ R/create_email_creds_file.R
@@ -2,13 +2,15 @@
 #' @description Creates a file with access
 #' credentials for the purpose of automatically
 #' emailing notification messages.
-#' @param file a file path for the credentials file
-#' to be stored on disk.
-#' @param sender the sender name.
+#' @param provider an optional provider email provider
+#' with which an STMP account is available. Options
+#' include \code{gmail}, \code{outlook},
+#' \code{office365}, and \code{icloud}.
 #' @param host the \code{host} name.
-#' @param port the port number.
 #' @param user the username for the email account.
 #' @param password the password associated with the
+#' @param sender the sender name.
+#' @param port the port number.
 #' \code{user}'s email address.
 #' @param use_ssl an option as to whether to use
 #' SSL; supply a \code{TRUE} or \code{FALSE}
@@ -30,14 +32,15 @@
 #'   user = "have_a@blastula.org",
 #'   password = "************")
 #' }
+#' @importFrom stringr str_replace_all
 #' @export create_email_creds_file
 
-create_email_creds_file <- function(file,
-                                    sender,
-                                    host,
-                                    port,
-                                    user,
-                                    password,
+create_email_creds_file <- function(provider = NULL,
+                                    host = NULL,
+                                    user = NULL,
+                                    password = NULL,
+                                    sender = NULL,
+                                    port = NULL,
                                     use_ssl = TRUE,
                                     use_tls = FALSE,
                                     authenticate = TRUE) {
@@ -47,9 +50,29 @@ create_email_creds_file <- function(file,
     stop("The value supplied to `use_ssl` must be TRUE or FALSE.")
   }
 
+  # Ensure that `use_tls` is either TRUE or FALSE
+  if (!(use_tls %in% c(TRUE, FALSE))) {
+    stop("The value supplied to `use_tls` must be TRUE or FALSE.")
+  }
+
   # Ensure that `authenticate` is either TRUE or FALSE
   if (!(authenticate %in% c(TRUE, FALSE))) {
     stop("The value supplied to `authenticate` must be TRUE or FALSE.")
+  }
+
+  # If a `provider` name is given, extract values for `host`,
+  # `port`, `use_ssl`, `use_tls`, and `authenticate`
+  if (!is.null(provider) &&
+      provider %in% (smtp_settings() %>% dplyr::pull(short_name))) {
+
+    settings_record <- smtp_settings() %>% dplyr::filter(short_name == provider)
+
+    # Extract settings for the provider
+    host <- settings_record %>% dplyr::pull(server)
+    port <- settings_record %>% dplyr::pull(port)
+    use_ssl <- settings_record %>% dplyr::pull(use_ssl)
+    use_tls <- settings_record %>% dplyr::pull(use_tls)
+    authenticate <- settings_record %>% dplyr::pull(authenticate)
   }
 
   # Collect all credential values into a
@@ -63,6 +86,15 @@ create_email_creds_file <- function(file,
     use_ssl = as.character(use_ssl),
     use_tls = as.character(use_tls),
     authenticate = as.character(authenticate))
+
+  # Construct a file name
+  file <-
+    paste0(
+      ".bls_",
+      stringr::str_replace_all(
+        string = host,
+        pattern = "\\.",
+        replacement = "_"))
 
   # Save the credential values as a file
   saveRDS(credentials, file = file)
