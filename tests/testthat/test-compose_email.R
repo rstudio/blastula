d2bbb5106e2e359942f7a3fafcc7e532f5d88ca6
Richard Iannone <riannone@me.com>
2018-06-12 14:40:36 -0500

    Add tests for messages with local inline images

--- tests/testthat/test-compose_email.R
+++ tests/testthat/test-compose_email.R
@@ -31,3 +31,66 @@ test_that("composing a simple message is possible", {
     object = email[[2]],
     class = "character")
 })
+
+
+test_that("composing a message with local inline images is possible", {
+
+  library(ggplot2)
+
+  # Create a ggplot plot
+  plot <-
+    ggplot(
+      data = mtcars,
+      aes(x = disp, y = hp,
+          color = wt, size = mpg)) +
+    geom_point()
+
+  # Convert the plot to an HTML fragment
+  plot_html <-
+    add_ggplot(
+      plot,
+      height = 5, width = 7)
+
+  # Create a simple email message with no text
+  email <-
+    compose_email(body = "
+  Here is a plot:
+
+  {plot_html}
+  ")
+
+  # Expect an object of class `email_message`
+  expect_is(
+    object = email,
+    class = "email_message")
+
+  # Expect the object to be a list of length 3
+  expect_equal(
+    length(email),
+    3)
+
+  # Expect specific names for each of
+  # the list components
+  expect_equal(
+    names(email),
+    c("html_str", "html_html", "images"))
+
+  # Expect that the first two components
+  # are of class `character`
+  expect_is(
+    object = email[[1]],
+    class = "character")
+
+  expect_is(
+    object = email[[2]],
+    class = "character")
+
+
+  expect_is(
+    object = email[[3]],
+    class = "list")
+
+  expect_is(
+    object = email[[3]][[1]],
+    class = "character")
+})
