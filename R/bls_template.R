83b4edf7009e5baf41097de01a309d531528e14f
Richard Iannone <riannone@me.com>
2019-03-18 22:48:29 -0400

    Modify the `bls_standard_template()` function

--- R/bls_template.R
+++ R/bls_template.R
@@ -320,11 +320,7 @@ bls_standard_template <- function() {
             <!-- START FOOTER -->
             <div class=\"footer\" style=\"clear: both; padding-top: 24px; text-align: center; width: 100%;\">
               <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;\" width=\"100%\">
-                <tr>
-                  <td class=\"content-block\" style=\"font-family: Helvetica, sans-serif; vertical-align: top; padding-top: 0; padding-bottom: 24px; font-size: 12px; color: #999999; text-align: center;\" valign=\"top\" align=\"center\">
-                    {html_footer}
-                  </td>
-                </tr>
+                {html_footer}
               </table>
             </div>
             <!-- END FOOTER -->
