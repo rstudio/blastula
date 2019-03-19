5a0fe7d6e4eb077a1246bfdc87b0175e92fa5207
Richard Iannone <riannone@me.com>
2019-03-18 22:48:10 -0400

    Add the `render_blocks()` function

---
+++ R/render_blocks.R
@@ -0,0 +1,23 @@
+render_blocks <- function(blocks, context = "body") {
+
+  for (i in seq_along(blocks)) {
+
+    if (inherits(blocks[[i]], "block_articles")) {
+      blocks[[i]] <- render_block_articles(blocks[[i]])
+    }
+
+    if (inherits(blocks[[i]], "block_text")) {
+      blocks[[i]] <- render_block_text(blocks[[i]], context = context)
+    }
+
+    if (inherits(blocks[[i]], "block_title")) {
+      blocks[[i]] <- render_block_title(blocks[[i]], context = context)
+    }
+
+    if (inherits(blocks[[i]], "block_social_links")) {
+      blocks[[i]] <- render_block_social_links(blocks[[i]])
+    }
+  }
+
+  blocks
+}
