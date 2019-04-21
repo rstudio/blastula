9136abd5194cd5a97eea11dbe8c3549eb85bfb58
Richard Iannone <riannone@me.com>
2019-04-20 23:30:59 -0400

    Add `time` argument to `add_readable_time()`

--- R/add_readable_time.R
+++ R/add_readable_time.R
@@ -4,6 +4,8 @@
 #' this helper function. This will insert the current date/time/tz based on the
 #' caller's locale information at the time of the call. There are options to
 #' specify whether the date, time, and time zone parts are to be included.
+#' @param time The POSIXct time to use, and to make more readable for email
+#'   recipients. If this is not provided, the current system time will be used.
 #' @param use_date A logical value that indicates whether the current date
 #'   should be included.
 #' @param use_time A logical value that indicates whether the current time
@@ -13,21 +15,26 @@
 #' @return A character object that can be placed inside any message component
 #'   message wherever the function is called.
 #' @importFrom glue glue
-#' @importFrom stringr str_squish
 #' @export
-
-add_readable_time <- function(use_date = TRUE,
+add_readable_time <- function(time = NULL,
+                              use_date = TRUE,
                               use_time = TRUE,
                               use_tz = TRUE) {
+
+  if (is.null(time)) {
+    time <- Sys.time()
+  } else if (!inherits(time, "POSIXct")) {
+    stop("The `time` value must be a POSIXct date-time value.")
+  }
 
 
   if (use_date) {
     current_date <-
       paste0(
-        format(Sys.time(), "%A, %B "),
-        format(Sys.time(), "%d") %>% as.numeric(),
+        format(time, "%A, %B "),
+        format(time, "%d") %>% as.numeric(),
         ", ",
-        format(Sys.time(), "%Y"))
+        format(time, "%Y"))
   } else {
     current_date <- ""
   }
@@ -35,8 +42,8 @@ add_readable_time <- function(use_date = TRUE,
   if (use_time) {
     current_time <-
       paste0(
-        format(Sys.time(), "%l:%M") %>% stringr::str_squish(),
-        toupper(format(Sys.time(), " %p")))
+        gsub(" ", "", format(time, "%l:%M")),
+        toupper(format(time, " %p")))
 
     if (use_date) {
       current_time <- paste0(" at ", current_time)
@@ -47,9 +54,9 @@ add_readable_time <- function(use_date = TRUE,
   }
 
   if (use_tz & (use_date | use_time)) {
-    current_tz <- format(Sys.time(), " (%Z)")
+    current_tz <- format(time, " (%Z)")
   } else if (use_tz & (use_date == FALSE & use_time == FALSE)) {
-    current_tz <- format(Sys.time(), "%Z")
+    current_tz <- format(time, "%Z")
   } else {
     current_tz <- ""
   }
