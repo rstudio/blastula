0b735bafa9c0b1bcd6e9a67e4c4714308e468e91
Richard Iannone <riannone@me.com>
2019-05-05 23:37:35 -0400

    Modify spacing of HTML in `simple_body_block()`

--- R/compose_email.R
+++ R/compose_email.R
@@ -254,18 +254,17 @@ compose_email <- function(body = NULL,
 #' @noRd
 simple_body_block <- function() {
 
-"                <tr>
-                  <td class=\"wrapper\" style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;\">
-                    <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;\">
-                      <tbody>
-                        <tr>
-                          <td style=\"font-family: Helvetica, sans-serif; font-size: 14px; vertical-align: top;\">
-                            <p style=\"font-family: Helvetica, sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 16px;\">{html_paragraphs}</p>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                </tr>
-                "
+"<tr>
+<td class=\"wrapper\" style=\"font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;\">
+<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;\">
+<tbody>
+<tr>
+<td style=\"font-family: Helvetica, sans-serif; font-size: 14px; vertical-align: top;\">
+<p style=\"font-family: Helvetica, sans-serif; font-size: 14px; font-weight: normal; margin: 0; margin-bottom: 16px;\">{html_paragraphs}</p>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>"
 }
