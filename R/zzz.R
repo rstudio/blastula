684959ec14c8fb4ef7df3fb87ce8d5dedd171d23
Richard Iannone <riannone@me.com>
2019-05-16 20:12:52 -0400

    Add to `globalVariables()` list

--- R/zzz.R
+++ R/zzz.R
@@ -5,8 +5,17 @@ globalVariables(
   c(
     "authenticate",
     "email",
+    "host",
+    "key_name",
     "name",
-    "use_tls"
+    "port",
+    "sender",
+    "service",
+    "short_name",
+    "use_ssl",
+    "use_tls",
+    "user",
+    "username"
     )
   )
 
