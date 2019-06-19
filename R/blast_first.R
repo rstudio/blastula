c9d7839393fc2f5ce6465ac2234ebdd44b3cedcf
Richard Iannone <riannone@me.com>
2019-06-19 14:34:38 -0400

    Fix link to function in roxygen documentation

--- R/blast_first.R
+++ R/blast_first.R
@@ -1,7 +1,7 @@
 #' Setup function to get SMTP mail working effectively
 #'
 #' Run this function first to obtain SMTP mailing functionality for the
-#' [send_mail_out()] function. That function is deprecated and relies on a
+#' [send_email_out()] function. That function is deprecated and relies on a
 #' different binary for sending. Therefore, we shouldn't need to use this setup
 #' function.
 #'
