609bb3f196f9245b71353e6e87ed547fc3846bdc
Richard Iannone <riannone@me.com>
2019-09-03 18:14:04 -0400

    Update connect_render_email.R

--- R/connect_render_email.R
+++ R/connect_render_email.R
@@ -4,20 +4,23 @@ connect_render_email <- function(input,
                                  subject = NULL,
                                  attachments = NULL,
                                  attach_output = FALSE,
-                                 preview = TRUE) {
+                                 preview = TRUE,
+                                 quiet = TRUE,
+                                 envir = parent.frame()) {
 
   # Check that input is an .Rmd file
 
   connect_email(
     email = render_email(
       input = input,
-      connect_footer = TRUE,
-      envir = parent.frame(),
-      quiet = TRUE),
-    subject = NULL,
-    attachments = NULL,
-    attach_output = FALSE,
+      connect_footer = connect_footer,
+      envir = envir,
+      quiet = quiet
+    ),
+    subject = subject,
+    attachments = attachments,
+    attach_output = attach_output,
     text = NULL,
-    preview = TRUE)
-
+    preview = preview
+  )
 }
