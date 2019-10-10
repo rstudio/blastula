5db3d9aeeabdaadf817c4188395ffa71682fb61d
Richard Iannone <riannone@me.com>
2019-10-10 12:05:24 -0400

    Modify name of object in `{ }`

--- R/bls_template.R
+++ R/bls_template.R
@@ -7,7 +7,7 @@ bls_standard_template <- function() {
   <head>
     <meta name=\"viewport\" content=\"width=device-width\">
     <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">
-    <title>{title_text}</title>
+    <title>{title}</title>
     <style media=\"all\" type=\"text/css\">
     @media only screen and (max-width: 640px) {{
       .span-2,
