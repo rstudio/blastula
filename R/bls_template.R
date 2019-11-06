facae65e2e30b317f0f730b43f21c5c48ddba9f6
Richard Iannone <riannone@me.com>
2019-11-06 15:27:21 -0500

    Add a color rule for the nbsp <td>s (left and right)
    There can be a display issue when the non-breaking space in the left and right <td> elements can sometimes undergo conversion. We need the non-breaking space (a equivalent characters for both). The compromise here is to color the space (or it's replacement string) the same color as the background.

--- R/bls_template.R
+++ R/bls_template.R
@@ -149,7 +149,7 @@ bls_standard_template <-
   <body style=\"font-family: Helvetica, sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; background-color: #f6f6f6; margin: 0; padding: 0;\">
     <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"body\" style=\"border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;\" width=\"100%\" bgcolor=\"#f6f6f6\">
       <tr>
-        <td style=\"font-family: Helvetica, sans-serif; font-size: 14px; vertical-align: top;\" valign=\"top\">&nbsp;</td>
+        <td style=\"font-family: Helvetica, sans-serif; font-size: 14px; color: #f6f6f6; vertical-align: top;\" valign=\"top\">&nbsp;</td>
         <td class=\"container\" style=\"font-family: Helvetica, sans-serif; font-size: 14px; vertical-align: top; margin: 0 auto !important; max-width: 600px; padding: 0; padding-top: 24px; width: 600px;\" width=\"600\" valign=\"top\">
           <div class=\"content\" style=\"box-sizing: border-box; display: block; margin: 0 auto; max-width: 600px; padding: 0;\">
 
@@ -183,7 +183,7 @@ bls_standard_template <-
 
   <!-- END CENTERED WHITE CONTAINER --></div>
           </td>
-          <td style=\"font-family: sans-serif; font-size: 14px; vertical-align: top;\">&nbsp;</td>
+          <td style=\"font-family: sans-serif; font-size: 14px; color: #f6f6f6; vertical-align: top;\">&nbsp;</td>
         </tr>
       </table>
     </body>
