9b3d890b4065f392c91e39ec29394c5363d76939
Richard Iannone <riannone@me.com>
2019-11-26 22:39:16 -0500

    Limit testing of rmarkdown-based tests
    To only those systems with Pandoc 2

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,47 +2,52 @@ context("Utility functions work as expected")
 
 test_that("knitr_sidecar_prefix preconditions hold", {
 
-  # Ensure that rmarkdown assumptions still hold
-  rmd_path <- file.path(tempdir(), "test-utils.Rmd")
+  if (rmarkdown:::pandoc2.0()) {
 
-  file.copy(testthat::test_path("test-utils.Rmd"), rmd_path)
+    # Ensure that rmarkdown assumptions still hold
+    rmd_path <- file.path(tempdir(), "test-utils.Rmd")
 
-  rmarkdown::render(rmd_path, quiet = TRUE)
+    file.copy(testthat::test_path("test-utils.Rmd"), rmd_path)
+
+    rmarkdown::render(rmd_path, quiet = TRUE)
+  }
 })
 
 test_that("knitr_sidecar_prefix behavior", {
 
-  # fig.path isn't set
-  expect_null(knitr_sidecar_prefix(NULL))
-  expect_null(knitr_sidecar_prefix(NULL, condition = FALSE))
-  expect_null(knitr_sidecar_prefix(NULL, condition = TRUE))
-  expect_identical("default", knitr_sidecar_prefix("default"))
-  expect_identical("default", knitr_sidecar_prefix("default", condition = FALSE))
-  expect_identical("default", knitr_sidecar_prefix("default", condition = TRUE))
+  if (rmarkdown:::pandoc2.0()) {
 
-  # Explicitly provide an unusable fig.path
-  expect_null(knitr_sidecar_prefix(NULL, fig_path = "."))
-  expect_null(knitr_sidecar_prefix(NULL, condition = FALSE, fig_path = "."))
-  expect_null(knitr_sidecar_prefix(NULL, condition = TRUE, fig_path = "."))
-  expect_identical("default", knitr_sidecar_prefix("default", fig_path = "."))
-  expect_identical("default", knitr_sidecar_prefix("default", condition = FALSE, fig_path = "."))
-  expect_identical("default", knitr_sidecar_prefix("default", condition = TRUE, fig_path = "."))
+    # fig.path isn't set
+    expect_null(knitr_sidecar_prefix(NULL))
+    expect_null(knitr_sidecar_prefix(NULL, condition = FALSE))
+    expect_null(knitr_sidecar_prefix(NULL, condition = TRUE))
+    expect_identical("default", knitr_sidecar_prefix("default"))
+    expect_identical("default", knitr_sidecar_prefix("default", condition = FALSE))
+    expect_identical("default", knitr_sidecar_prefix("default", condition = TRUE))
 
-  # Usable fig.path provided
-  expect_identical(
-    knitr_sidecar_prefix(NULL, condition = TRUE,
-      fig_path = "a12 !_-x_files/b_files/figure-github_flavored_markdown/"),
-    "a12 !_-x_files/b"
-  )
-  expect_null(
-    knitr_sidecar_prefix(NULL, condition = FALSE,
-      fig_path = "a12 !_-x_files/b_files/figure-github_flavored_markdown/")
-  )
-  expect_null(
-    knitr_sidecar_prefix(NULL,
-      fig_path = "a12 !_-x_files/b_files/figure-github_flavored_markdown/")
-  )
+    # Explicitly provide an unusable fig.path
+    expect_null(knitr_sidecar_prefix(NULL, fig_path = "."))
+    expect_null(knitr_sidecar_prefix(NULL, condition = FALSE, fig_path = "."))
+    expect_null(knitr_sidecar_prefix(NULL, condition = TRUE, fig_path = "."))
+    expect_identical("default", knitr_sidecar_prefix("default", fig_path = "."))
+    expect_identical("default", knitr_sidecar_prefix("default", condition = FALSE, fig_path = "."))
+    expect_identical("default", knitr_sidecar_prefix("default", condition = TRUE, fig_path = "."))
 
+    # Usable fig.path provided
+    expect_identical(
+      knitr_sidecar_prefix(NULL, condition = TRUE,
+                           fig_path = "a12 !_-x_files/b_files/figure-github_flavored_markdown/"),
+      "a12 !_-x_files/b"
+    )
+    expect_null(
+      knitr_sidecar_prefix(NULL, condition = FALSE,
+                           fig_path = "a12 !_-x_files/b_files/figure-github_flavored_markdown/")
+    )
+    expect_null(
+      knitr_sidecar_prefix(NULL,
+                           fig_path = "a12 !_-x_files/b_files/figure-github_flavored_markdown/")
+    )
+  }
 })
 
 test_that("the `smtp_settings()` function returns the expected output", {
