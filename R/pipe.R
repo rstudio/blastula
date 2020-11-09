b6614d35c979c701da9112b38f17bffab2c16477
Richard Iannone <riannone@me.com>
2020-11-09 12:13:35 -0500

    Update pipe.R

--- R/pipe.R
+++ R/pipe.R
@@ -2,8 +2,10 @@
 #'
 #' The `blastula` package uses the pipe function, `\%>\%`, to turn function
 #' composition into a series of imperative statements.
+#'
 #' @importFrom magrittr %>%
 #' @name %>%
+#' @keywords internal
 #' @rdname pipe
 #' @export
 NULL
